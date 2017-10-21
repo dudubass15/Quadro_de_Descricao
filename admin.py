311e6868970fb536eca0091a2b11e90277e70d01
dudubass15 <carlos.contato00@gmail.com>
2017-10-21 14:05:51 -0200

    Reconfiguração projeto

--- admin.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from .models import Post_descricao
-from django.contrib import admin
-
-admin.site.register(Post_descricao)
--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^home', views.index, name = 'home'),
-    url(r'^adicionar', views.adicionar, name='adicionar'),
-]
