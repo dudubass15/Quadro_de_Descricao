1d3d769b244e5e60360d338ee69b4adf5a960695
dudubass15 <carlos.contato00@gmail.com>
2017-10-21 13:24:37 -0200

    Alteração dos arquivos .CSS

--- descricao/forms.py
+++ descricao/forms.py
@@ -2,10 +2,28 @@ from django import forms
 from .models import Post_descricao
 
 class PostForm(forms.Form):
-	apartamento = forms.IntegerField()
-	condominio = forms.CharField(label='Condominio', max_length=150)
-	descricao = forms.CharField(label='Descricao', widget=forms.Textarea)
-	liberacao = forms.CharField(max_length=200)
-	data_liberacao = forms.DateField()
-	nome_operador = forms.CharField(label='Operador', max_length=200)
-	central = forms.CharField(max_length=200)
+	apartamento = forms.IntegerField(
+		widget=forms.NumberInput(attrs={'class':'form-control'})
+	)
+	condominio = forms.CharField(max_length=150, 
+		widget=forms.TextInput(attrs={'class':'form-control'})
+	)
+	situacao_da_liberacao = forms.CharField(max_length=200, 
+		widget=forms.TextInput(attrs={'class':'form-control'})
+	)
+	data_liberacao = forms.DateField(
+		widget=forms.DateInput(attrs={'class':'form-control'})
+	)
+	operador = forms.CharField(max_length=200, 
+		widget=forms.TextInput(attrs={'class':'form-control'})
+	)
+	central = forms.CharField(max_length=200, 
+		widget=forms.TextInput(attrs={'class':'form-control'})
+	)
+	descricao = forms.CharField(max_length=200, 
+		widget=forms.Textarea(attrs={'class':'form-control'})
+	)
+
+	class Meta:
+		model = Post_descricao
+		fields = ['apartamento','condominio', 'situacao_da_liberacao', 'data_liberacao', 'nome_operador', 'central', 'descricao']
--- descricao/views.py
+++ descricao/views.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from .forms import PostForm
 
 
 def index(request):
     return render(request, 'index.html', {})
 
 def adicionar(request):
-    return render(request, 'inserir.html', {})
+	if request.method == "POST":
+	    form = PostForm(request.POST)
+	else:
+	    form = PostForm()
+
+	form = PostForm()
+    	return render(request, 'inserir.html', {'form': form})
