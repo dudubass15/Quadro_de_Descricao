a979b307a9229bf092328dc696b1f5d0fc509d76
dudubass15 <carlos.contato00@gmail.com>
2017-10-24 21:29:10 -0200

    terminado formulário

--- descricao/forms.py
+++ descricao/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+	apartamento = forms.IntegerField()
+	condominio = forms.CharField(max_length=150)
+	liberacao = forms.CharField(max_length=200)
+	data_liberacao = forms.DateField()
+	nome_operador = forms.CharField(max_length=200)
+	central = forms.CharField(max_length=200)
+	descricao = forms.CharField(max_length=300)
 
 	class Meta:
 		model = Post
