c301ee6197c04234d0395fdb4b96d73843f2d45d
dudubass15 <carlos.contato00@gmail.com>
2017-10-26 14:30:29 -0200

    Alteração forms.py

--- descricao/forms.py
+++ descricao/forms.py
@@ -4,11 +4,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
 	apartamento = forms.IntegerField()
 	condominio = forms.CharField(max_length=150)
-	liberacao = forms.CharField(max_length=200)
+	liberacao = forms.CharField(max_length=100)
 	data_liberacao = forms.DateField()
 	nome_operador = forms.CharField(max_length=200)
 	central = forms.CharField(max_length=200)
-	descricao = forms.CharField(max_length=300)
+	descricao = forms.TextInput()
 
 	class Meta:
 		model = Post
