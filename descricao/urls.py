589421564ab9b808c04192c17006b0d8eafd2008
dudubass15 <carlos.contato00@gmail.com>
2017-10-26 14:00:33 -0200

    Adicionado algumas views e urls

--- descricao/urls.py
+++ descricao/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^home', views.index, name = 'home'),
-    url(r'^adicionar', views.adicionar, name='adicionar'),
+    url(r'^home$', views.index, name = 'home'),
+    url(r'^adicionar$', views.adicionar, name='adicionar'),
+    url(r'^mossad$', views.mossad, name='mossad'),
+    url(r'^adicionar_mossad$', views.adicionar_mossad, name='adicionar_mossad'),
+    url(r'^mi5$', views.mi5, name='mi5'),
+    url(r'^adicionar_mi5$', views.adicionar_mi5, name='adicionar_mi5'),
 ]
--- descricao/views.py
+++ descricao/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 
 def index(request):
-	post = Post.objects.all()
+	post = Post.objects.all() #Aqui chama todos os objetos do banco e passa para o template (index)
 
 	return render(request, 'index.html', {'post': post})
 
@@ -20,3 +20,36 @@ def adicionar(request):
 	    form = PostForm()
 
     	return render(request, 'inserir.html', {'form': form})
+
+
+def mossad(request):
+	post = Post.objects.filter(central='Mossad')
+
+	return render(request, 'mossad.html', {'post': post})
+
+def adicionar_mossad(request):
+	if request.method == "POST":
+	    form = PostForm(request.POST)
+	    if form.is_valid():
+	    	form.save()
+	    	return HttpResponseRedirect('mossad') # Redireciona depois do POST
+	else:
+	    form = PostForm()
+
+	return render(request, 'inserir_mossad.html', {'form': form})
+
+def mi5(request):
+	post = Post.objects.filter(central='MI5')
+
+	return render(request, 'mi5.html', {'post': post})
+
+def adicionar_mi5(request):
+	if request.method == "POST":
+	    form = PostForm(request.POST)
+	    if form.is_valid():
+	    	form.save()
+	    	return HttpResponseRedirect('mi5') # Redireciona depois do POST
+	else:
+	    form = PostForm()
+
+	return render(request, 'inserir_mi5.html', {'form': form})
